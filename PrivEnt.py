b37b3342fd910ee99b333142f4e7ffc1630a34b4
Ozik <tech.emu@gmail.com>
2017-12-31 15:26:08 +0200

    parsing works and saves to the db (previously created)

--- PrivEnt.py
+++ PrivEnt.py
@@ -6,7 +6,7 @@ from utilities import general
 def main(database: str, db_action: str, xml_file: str):
     print("database is: " + database)
     print("action is: " + db_action)
-    print("name to find: " + xml_file)
+    print("file to export: " + xml_file)
     start = round(time.clock(),1)
     general.parse_xml(xml_file, database)
     end = round(time.clock(),1)
@@ -14,13 +14,13 @@ def main(database: str, db_action: str, xml_file: str):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Utility for working with Private Entrepreneur DB')
     parser.add_argument("-db", "--database", help="SQLite file name")
     parser.add_argument("-a", "--action", help="Action with database")
     parser.add_argument("-x", "--xmlfile", help="File to import data from")
     # TODO: parser.add_argument("-f", "--action", help="File to import data from")
     args = parser.parse_args()
-    database_file = args.database
-    database_action = args.action
-    xml_file = args.xmlfile
+    database_file = "" #args.database
+    # database_action = args.action
+    # xml_file = args.xmlfile
     main(database_file, database_action, xml_file)
